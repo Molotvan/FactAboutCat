8a2b96efbf43c4ec6a9818eb7b7d6ead9a806f86
Molotvan <molotvan@yandex.ru>
2022-06-20 23:54:26 +0300

    initial

---
+++ src/main/java/FactAboutCat.java
@@ -0,0 +1,57 @@
+public class FactAboutCat {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private Integer upvotes;
+
+    public FactAboutCat(){}
+
+    public FactAboutCat(String id, String text, String type,String user, Integer upvotes ){
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+    public String getId(){
+        return id;
+    }
+
+    public String getText(){
+        return text;
+    }
+
+    public String getType(){
+        return type;
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public Integer getUpvotes(){
+        return upvotes;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public void setText(String text){
+        this.text = text;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public void setUser(String user){
+        this.user = user;
+    }
+
+    public void setUpvotes(Integer upvotes){
+        this.upvotes = upvotes;
+    }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.ContentType;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.util.List;
+
+
+
+public class Main {
+    public static ObjectMapper mapper = new ObjectMapper();
+      public static String REMOTE_SERVER_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+      public static void main(String[] args) throws IOException {
+          CloseableHttpClient httpClient = HttpClients.createDefault();
+          HttpGet request = new HttpGet(REMOTE_SERVER_URI);
+          CloseableHttpResponse response = httpClient.execute(request);
+          System.out.println(response.getStatusLine());
+          List<FactAboutCat> FAC = mapper.readValue(response.getEntity().getContent(), new TypeReference<>(){});
+          FAC.stream()
+                  .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                  .forEach(value -> System.out.println(value.getText()));
+      }
+
+}
